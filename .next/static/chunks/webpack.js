4d3a937ba7246b577692fc50bfd9573ff18b1151
hootmole <jachym1137@gmail.com>
2022-12-05 17:30:58 +0100

    a

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7e6457555b3fbb21"; }
+/******/ 		__webpack_require__.h = function() { return "94b91e58c994354f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,10 +5,10 @@ import React from "react"
 
 export function Hero() {
     return (
-        <div className=" w-screen h-[5000px] pt-60">
-            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos labore quo est perspiciatis fugiat a natus cumque at animi. Corporis aperiam nobis labore iure odit omnis repellendus enim obcaecati minus.</h2>
-            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quae nesciunt ea optio aliquid est nam voluptate repellendus at veritatis, quasi magnam tempore sint, dolor placeat molestias facere harum rerum.</h4>
-            <h4>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam unde molestiae, sapiente eveniet, aut iusto, voluptas quibusdam impedit mollitia debitis perferendis! Deleniti suscipit nisi incidunt quasi maiores rem. Deleniti, adipisci!</h4>
+        <div className="">
+            <div className=" w-screen h-[600px] bg-neutral-600">
+                
+            </div>
         </div>
     )
 }
